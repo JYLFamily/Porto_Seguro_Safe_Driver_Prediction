7a2727a0fdc7c5210224aafe8ef52a385ec388ef
1579502635@qq.com <1992720jyl>
2019-05-20 08:01:35 +0800

    FIX BUG

--- EntityEmbeddingNeuralNet/EntityEmbeddingNeuralNet.py
+++ EntityEmbeddingNeuralNet/EntityEmbeddingNeuralNet.py
@@ -4,15 +4,17 @@ import os
 import yaml
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 from tensorflow import set_random_seed
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from keras.models import Model
-from keras.layers import Input, Embedding, Reshape, Concatenate, Dropout, Dense
+from keras.layers import Input, Embedding, Reshape, Concatenate, Dense, BatchNormalization
 from keras.utils.vis_utils import plot_model
-from keras.callbacks import EarlyStopping
 from sklearn.model_selection import StratifiedKFold
-from tqdm import tqdm
+from keras.callbacks import EarlyStopping
+from keras.optimizers import Adam
 np.random.seed(7)
 set_random_seed(7)
 pd.set_option("display.max_row", None)
@@ -82,7 +84,7 @@ class EntityEmbeddingNeuralNet(object):
                     mode = self.__train_feature[col].value_counts().index[0]
                     categories = self.__train_feature[col].unique()
 
-                    self.__test_feature[col] = self.__test_feature[col].fillna(mode)
+                    self.__test_feature[col] = self.__test_feature[col].fillna("missing")
                     self.__test_feature[col] = [mode if i not in categories else i for i in self.__test_feature[col]]
 
                 else:  # train 不存在缺失
@@ -114,68 +116,71 @@ class EntityEmbeddingNeuralNet(object):
 
         # categorical feature
         for col, num in self.__categorical_columns_num_unique.items():
-            input_layer = Input(shape=(1, ), name=col+"_input")
+            input_layer = Input(shape=(1, ), name=col + "_input")
             embedding_layer = Embedding(
-                input_dim=num, output_dim=min(50, num // 2), input_length=1, name=col+"_embedding")(input_layer)
-            embedding_layer = Reshape(target_shape=(min(50, num // 2), ))(embedding_layer)
+                input_dim=num, output_dim=min(10, num // 2), input_length=1, name=col + "_embedding")(input_layer)
+            embedding_layer = Reshape(target_shape=(min(10, num // 2), ), name=col + "_reshape")(embedding_layer)
             input_layers.append(input_layer)
             embedding_layers.append(embedding_layer)
 
         # numeric feature
-        input_layer = Input(shape=(len(self.__train_feature.columns) - len(self.__categorical_columns_num_unique), ))
+        input_layer = Input(
+            shape=(len(self.__train_feature.columns) - len(self.__categorical_columns_num_unique), ),
+            name="numeric_input")
         input_layers.append(input_layer)
 
-        hidden_layer_categorical = Dropout(0.02)(Concatenate()(embedding_layers))
-        hidden_layer_categorical = Dense(40, activation="relu")(hidden_layer_categorical)
-        hidden_layer_numeric = Dropout(0.02)(input_layer)
-        hidden_layer_numeric = Dense(40, activation="relu")(hidden_layer_numeric)
-        hidden_layer = Concatenate()([hidden_layer_categorical, hidden_layer_numeric])
-
-        hidden_layer = Dense(40, activation="relu")(hidden_layer)
-        hidden_layer = Dropout(.35)(hidden_layer)
-        hidden_layer = Dense(20, activation="relu")(hidden_layer)
-        hidden_layer = Dropout(.15)(hidden_layer)
-        hidden_layer = Dense(10, activation="relu")(hidden_layer)
-        hidden_layer = Dropout(.15)(hidden_layer)
-        output_layer = Dense(1, activation="sigmoid")(hidden_layer)
+        hidden_layer = Dense(units=16, activation="relu")(Concatenate()([Concatenate()(embedding_layers), input_layer]))
+        hidden_layer = BatchNormalization()(hidden_layer)
+        hidden_layer = Dense(units=8, activation="relu")(hidden_layer)
+        hidden_layer = BatchNormalization()(hidden_layer)
+        hidden_layer = Dense(units=4, activation="relu")(hidden_layer)
+        hidden_layer = BatchNormalization()(hidden_layer)
+        output_layer = Dense(1, activation="sigmoid", name="output_layer")(hidden_layer)
 
         self.__net = Model(input_layers, output_layer)
-        plot_model(
-            self.__net, to_file=os.path.join(self.__output_path, "model_plot.png"), show_shapes=True, show_layer_names=True)
+        plot_model(self.__net, to_file="net.png", show_shapes=True, show_layer_names=True)
 
         # stacking
-        # self.__folds = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
-        # self.__sub_preds = np.zeros(shape=self.__test_feature.shape[0])
-        #
-        # for n_fold, (trn_idx, val_idx) in enumerate(self.__folds.split(X=self.__train_feature, y=self.__train_label)):
-        #     trn_x, trn_y = self.__train_feature.iloc[trn_idx], self.__train_label.iloc[trn_idx]
-        #     val_x, val_y = self.__train_feature.iloc[val_idx], self.__train_label.iloc[val_idx]
-        #
-        #     tra_feature_for_model = []
-        #     val_feature_for_model = []
-        #
-        #     for col in self.__categorical_columns:
-        #         tra_feature_for_model.append(trn_x[col])
-        #         val_feature_for_model.append(val_x[col])
-        #
-        #     tra_feature_for_model.append(trn_x[self.__numeric_columns].values)
-        #     val_feature_for_model.append(val_x[self.__numeric_columns].values)
-        #
-        #     self.__early_stopping = EarlyStopping(patience=4, verbose=1)
-        #
-        #     self.__net.compile(loss="binary_crossentropy", optimizer="adam")
-        #     self.__net.fit(
-        #         tra_feature_for_model,
-        #         trn_y.values,
-        #         epochs=20,
-        #         batch_size=32,
-        #         validation_data=(val_feature_for_model, val_y.values),
-        #         callbacks=[self.__early_stopping]
-        #     )
-        #     pred_test = self.__net.predict(self.__test_feature_for_model)  # 2D shape
-        #     self.__sub_preds += pred_test.reshape((-1, )) / self.__folds.n_splits
-        #
-        # self.__test_id["target"] = self.__sub_preds
+        self.__folds = StratifiedKFold(n_splits=5, shuffle=True, random_state=7)
+        self.__sub_preds = np.zeros(shape=(self.__test_feature.shape[0], ))
+
+        for n_fold, (trn_idx, val_idx) in enumerate(self.__folds.split(X=self.__train_feature, y=self.__train_label)):
+            trn_x, trn_y = self.__train_feature.iloc[trn_idx], self.__train_label.iloc[trn_idx]
+            val_x, val_y = self.__train_feature.iloc[val_idx], self.__train_label.iloc[val_idx]
+
+            tra_feature_for_model = []
+            val_feature_for_model = []
+
+            for col in self.__categorical_columns:
+                tra_feature_for_model.append(trn_x[col])
+                val_feature_for_model.append(val_x[col])
+
+            tra_feature_for_model.append(trn_x[self.__numeric_columns].values)
+            val_feature_for_model.append(val_x[self.__numeric_columns].values)
+
+            self.__early_stopping = EarlyStopping(patience=4, verbose=2)
+            self.__net.compile(loss="binary_crossentropy", optimizer=Adam())
+
+            history = self.__net.fit(
+                tra_feature_for_model,
+                trn_y.values,
+                epochs=20,
+                batch_size=32,
+                validation_data=(val_feature_for_model, val_y.values),
+                callbacks=[self.__early_stopping]
+            )
+            plt.plot(history.history["loss"])
+            plt.plot(history.history["val_loss"])
+            plt.title("model loss")
+            plt.ylabel("loss")
+            plt.xlabel("epoch")
+            plt.legend(["train", "test"], loc="upper left")
+            plt.show()
+
+            pred_test = self.__net.predict(self.__test_feature_for_model)  # 2D shape
+            self.__sub_preds += pred_test.reshape((-1, )) / self.__folds.n_splits
+
+        self.__test_id["target"] = self.__sub_preds
 
     def data_write(self):
         self.__test_id.to_csv(os.path.join(self.__output_path, "sample_submission.csv"), index=False)
@@ -189,7 +194,7 @@ if __name__ == "__main__":
     eenn.data_read()
     eenn.data_prepare()
     eenn.model_fit_predict()
-    # eenn.data_write()
+    eenn.data_write()
 
 
 
